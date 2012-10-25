d533ec92b721d9e42ea1b8a0a781f10c00c54930
Kiall Mac Innes <kiall@managedit.ie>
2012-10-25 21:40:23 +0100

    Backport a few of assert methods not present in Py26. Fixes the tests on Py26

--- moniker/tests/__init__.py
+++ moniker/tests/__init__.py
@@ -13,6 +13,7 @@
 # WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 # License for the specific language governing permissions and limitations
 # under the License.
+import sys
 import unittest
 import mox
 from moniker.openstack.common import cfg
@@ -45,3 +46,38 @@ class TestCase(unittest.TestCase):
 
     def get_admin_context(self):
         return get_admin_context()
+
+
+if sys.version_info < (2, 7):
+    # Add in some of the nicer methods not present in 2.6
+    from contextlib import contextmanager
+
+    def assertIsNone(self, expr, msg=None):
+        return self.assertEqual(expr, None, msg)
+
+    TestCase.assertIsNone = assertIsNone
+
+    def assertIsNotNone(self, expr, msg=None):
+        return self.assertNotEqual(expr, None, msg)
+
+    TestCase.assertIsNotNone = assertIsNotNone
+
+    def assertRaises(self, excClass, callableObj=None, *args, **kwargs):
+        @contextmanager
+        def context():
+            raised = None
+            try:
+                yield
+            except Exception, e:
+                raised = e
+            finally:
+                if not isinstance(raised, excClass):
+                    raise self.failureException(
+                        "%s not raised" % str(excClass))
+
+        if callableObj is None:
+            return context()
+        with context:
+            callableObj(*args, **kwargs)
+
+    TestCase.assertRaises = assertRaises
