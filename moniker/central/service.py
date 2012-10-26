6f5cc691e82500edfffff70e290340b73ea9d7b2
Patrick Galbraith <patg@hp.com>
2012-10-26 16:26:43 +0000

    Found a bug that allows insertion into domains with NULL tenant_id

--- moniker/central/service.py
+++ moniker/central/service.py
@@ -69,6 +69,9 @@ class Service(rpc_service.Service):
     # Domain Methods
     def create_domain(self, context, values):
         values['tenant_id'] = context.tenant
+        # DB table defines this column "NOT NULL"
+        if values['tenant_id'] is None:
+            values['tenant_id'] = '' 
 
         domain = self.storage_conn.create_domain(context, values)
 
