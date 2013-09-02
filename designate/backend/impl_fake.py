003e7e3ca3e78cc96d13829776ca579815962bf2
Graham Hayes <graham@hayes.ie>
2013-09-02 16:17:15 +0100

    Cleaned up fake backend backend
    removed un-used calls
    
    Change-Id: I81b4ee27aa63ccf0ac35a3ae1ed72c3115da7b2d

--- designate/backend/impl_fake.py
+++ designate/backend/impl_fake.py
@@ -25,67 +25,38 @@ class FakeBackend(base.Backend):
     def __init__(self, *args, **kwargs):
         super(FakeBackend, self).__init__(*args, **kwargs)
 
-        self.create_tsigkey_calls = []
-        self.update_tsigkey_calls = []
-        self.delete_tsigkey_calls = []
-        self.create_domain_calls = []
-        self.update_domain_calls = []
-        self.delete_domain_calls = []
-        self.create_record_calls = []
-        self.update_record_calls = []
-        self.delete_record_calls = []
-        self.sync_domain_calls = []
-        self.sync_record_calls = []
-        self.ping_calls = []
-
     def create_tsigkey(self, context, tsigkey):
         LOG.info('Create TSIG Key %r' % tsigkey)
-        self.create_tsigkey_calls.append((context, tsigkey))
 
     def update_tsigkey(self, context, tsigkey):
         LOG.info('Update TSIG Key %r' % tsigkey)
-        self.update_tsigkey_calls.append((context, tsigkey))
 
     def delete_tsigkey(self, context, tsigkey):
         LOG.info('Delete TSIG Key %r' % tsigkey)
-        self.delete_tsigkey_calls.append((context, tsigkey))
 
     def create_domain(self, context, domain):
         LOG.info('Create Domain %r' % domain)
-        self.create_domain_calls.append((context, domain))
 
     def update_domain(self, context, domain):
         LOG.debug('Update Domain %r' % domain)
-        self.update_domain_calls.append((context, domain))
 
     def delete_domain(self, context, domain):
         LOG.debug('Delete Domain %r' % domain)
-        self.delete_domain_calls.append((context, domain))
 
     def create_record(self, context, domain, record):
         LOG.debug('Create Record %r / %r' % (domain, record))
-        self.create_record_calls.append((context, domain, record))
 
     def update_record(self, context, domain, record):
         LOG.debug('Update Record %r / %r' % (domain, record))
-        self.update_record_calls.append((context, domain, record))
 
     def delete_record(self, context, domain, record):
         LOG.debug('Delete Record %r / %r' % (domain, record))
-        self.delete_record_calls.append((context, domain, record))
 
     def sync_domain(self, context, domain, records):
         LOG.debug('Sync Domain %r / %r' % (domain, records))
-        self.sync_domain_calls.append((context, domain, records))
 
     def sync_record(self, context, domain, record):
         LOG.debug('Sync Record %r / %r' % (domain, record))
-        self.sync_record_calls.append((context, domain, record))
 
     def ping(self, context):
         LOG.debug('Ping')
-        self.ping_calls.append((context))
-
-        return {
-            'status': True
-        }
