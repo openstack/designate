5a6795d7c8640dcb49e58f5cfa70afdbad267e86
Vinod Mangalpally <vinod.mang@rackspace.com>
2013-11-20 12:38:06 -0600

    Add SLDs that act like TLDs
    This change is from the Icehouse release. The effective TLD file need
    not be present. When it is not present the behavior would be the same
    as before. When it is present, the domains that are being created are
    checked to see that it is not an effective TLD.
    
    This change offers the designate administrator the ability to configure
    2 lists to indicate the TLDs(accepted_tlds_file) and SLDs and others
    that effectively act as TLDs.(effective_tlds_file)
    
    2 sample files are included to show the format required for these files.
    The TLD file is from http://data.iana.org/TLD/tlds-alpha-by-domain.txt
    Commented lines in this file begin with a #. There is one entry per line
    and the entries are in the IDNA format.
    
    The sample effective TLD file is from http://publicsuffix.org/list/
    This file is in the UTF-8 format. Lines beginning with a // and ! are
    ignored. The wildcard character * (asterisk) may only be used to
    wildcard the topmost level in a domain name. All the entries are
    converted to idna format.
    
    Change-Id: I3d2f7e2741f4cda0b227e4a5671b141f99052291
    Closes-Bug: #1249396

--- designate/central/__init__.py
+++ designate/central/__init__.py
@@ -15,45 +15,6 @@
 # under the License.
 from oslo.config import cfg
 
-# NOTE(kiall): See http://data.iana.org/TLD/tlds-alpha-by-domain.txt
-#              Version 2013110700.
-IANA_TLDS = [
-    'ac', 'ad', 'ae', 'aero', 'af', 'ag', 'ai', 'al', 'am', 'an', 'ao', 'aq',
-    'ar', 'arpa', 'as', 'asia', 'at', 'au', 'aw', 'ax', 'az', 'ba', 'bb', 'bd',
-    'be', 'bf', 'bg', 'bh', 'bi', 'biz', 'bj', 'bm', 'bn', 'bo', 'br', 'bs',
-    'bt', 'bv', 'bw', 'by', 'bz', 'ca', 'camera', 'cat', 'cc', 'cd', 'cf',
-    'cg', 'ch', 'ci', 'ck', 'cl', 'clothing', 'cm', 'cn', 'co', 'com', 'coop',
-    'cr', 'cu', 'cv', 'cw', 'cx', 'cy', 'cz', 'de', 'dj', 'dk', 'dm', 'do',
-    'dz', 'ec', 'edu', 'ee', 'eg', 'equipment', 'er', 'es', 'et', 'eu', 'fi',
-    'fj', 'fk', 'fm', 'fo', 'fr', 'ga', 'gb', 'gd', 'ge', 'gf', 'gg', 'gh',
-    'gi', 'gl', 'gm', 'gn', 'gov', 'gp', 'gq', 'gr', 'gs', 'gt', 'gu', 'guru',
-    'gw', 'gy', 'hk', 'hm', 'hn', 'holdings', 'hr', 'ht', 'hu', 'id', 'ie',
-    'il', 'im', 'in', 'info', 'int', 'io', 'iq', 'ir', 'is', 'it', 'je',
-    'jm', 'jo', 'jobs', 'jp', 'ke', 'kg', 'kh', 'ki', 'km', 'kn', 'kp', 'kr',
-    'kw', 'ky', 'kz', 'la', 'lb', 'lc', 'li', 'lighting', 'lk', 'lr', 'ls',
-    'lt', 'lu', 'lv', 'ly', 'ma', 'mc', 'md', 'me', 'mg', 'mh', 'mil', 'mk',
-    'ml', 'mm', 'mn', 'mo', 'mobi', 'mp', 'mq', 'mr', 'ms', 'mt', 'mu',
-    'museum', 'mv', 'mw', 'mx', 'my', 'mz', 'na', 'name', 'nc', 'ne', 'net',
-    'nf', 'ng', 'ni', 'nl', 'no', 'np', 'nr', 'nu', 'nz', 'om', 'org', 'pa',
-    'pe', 'pf', 'pg', 'ph', 'pk', 'pl', 'pm', 'pn', 'post', 'pr', 'pro', 'ps',
-    'pt', 'pw', 'py', 'qa', 're', 'ro', 'rs', 'ru', 'rw', 'sa', 'sb', 'sc',
-    'sd', 'se', 'sg', 'sh', 'si', 'singles', 'sj', 'sk', 'sl', 'sm', 'sn',
-    'so', 'sr', 'st', 'su', 'sv', 'sx', 'sy', 'sz', 'tc', 'td', 'tel', 'tf',
-    'tg', 'th', 'tj', 'tk', 'tl', 'tm', 'tn', 'to', 'tp', 'tr', 'travel', 'tt',
-    'tv', 'tw', 'tz', 'ua', 'ug', 'uk', 'us', 'uy', 'uz', 'va', 'vc', 've',
-    'ventures', 'vg', 'vi', 'vn', 'voyage', 'vu', 'wf', 'ws', 'xn--3e0b707e',
-    'xn--45brj9c', 'xn--80ao21a', 'xn--80asehdb', 'xn--80aswg', 'xn--90a3ac',
-    'xn--clchc0ea0b2g2a9gcd', 'xn--fiqs8s', 'xn--fiqz9s', 'xn--fpcrj9c3d',
-    'xn--fzc2c9e2c', 'xn--gecrj9c', 'xn--h2brj9c', 'xn--j1amh', 'xn--j6w193g',
-    'xn--kprw13d', 'xn--kpry57d', 'xn--l1acc', 'xn--lgbbat1ad8j',
-    'xn--mgb9awbf', 'xn--mgba3a4f16a', 'xn--mgbaam7a8h', 'xn--mgbayh7gpa',
-    'xn--mgbbh1a71e', 'xn--mgbc0a9azcg', 'xn--mgberp4a5d4ar', 'xn--mgbx4cd0ab',
-    'xn--ngbc5azd', 'xn--o3cw4h', 'xn--ogbpf8fl', 'xn--p1ai', 'xn--pgbs0dh',
-    'xn--s9brj9c', 'xn--unup4y', 'xn--wgbh1c', 'xn--wgbl6a',
-    'xn--xkc2al3hye2a', 'xn--xkc2dl3a5ee0h', 'xn--yfro4i67o', 'xn--ygbi2ammx',
-    'xxx', 'ye', 'yt', 'za', 'zm', 'zw'
-]
-
 cfg.CONF.register_group(cfg.OptGroup(
     name='service:central', title="Configuration for Central Service"
 ))
@@ -71,8 +32,10 @@ cfg.CONF.register_opts([
                 default=['\\.arpa\\.$', '\\.novalocal\\.$', '\\.localhost\\.$',
                          '\\.localdomain\\.$', '\\.local\\.$'],
                 help='DNS domain name blacklist'),
-    cfg.ListOpt('accepted-tld-list', default=IANA_TLDS,
-                help='Accepted TLDs'),
+    cfg.StrOpt('accepted-tlds-file', default='tlds-alpha-by-domain.txt',
+               help='Accepted TLDs'),
+    cfg.StrOpt('effective-tlds-file', default='effective_tld_names.dat',
+               help='Effective TLDs'),
     cfg.IntOpt('max_domain_name_len', default=255,
                help="Maximum domain name length"),
     cfg.IntOpt('max_record_name_len', default=255,
