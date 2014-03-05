490d6b36361b6980d29d2a927fa2596602338dcc
Endre Karlson <endre.karlson@hp.com>
2014-03-05 11:50:07 +0100

    Introduce nameservers endpoint for zones
    This adds /zones/<uuid>/nameservers according the the bug report and
    will later support server-pools in Juno.
    
    Closes-Bug: #1281839
    
    Change-Id: If5ba284320e0e72ca25d7b288ddf0f169cc297a1

---
+++ designate/api/v2/controllers/nameservers.py
@@ -0,0 +1,39 @@
+# Copyright 2013 Hewlett-Packard Development Company, L.P.
+#
+# Author: Kiall Mac Innes <kiall@hp.com>
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+import pecan
+from designate import utils
+from designate.central import rpcapi as central_rpcapi
+from designate.openstack.common import log as logging
+from designate.api.v2.controllers import rest
+from designate.api.v2.views import nameservers as nameservers_view
+
+LOG = logging.getLogger(__name__)
+central_api = central_rpcapi.CentralAPI()
+
+
+class NameServersController(rest.RestController):
+    _view = nameservers_view.NameServerView()
+
+    @pecan.expose(template='json:', content_type='application/json')
+    @utils.validate_uuid('zone_id')
+    def get_all(self, zone_id):
+        request = pecan.request
+        context = pecan.request.environ['context']
+
+        servers = central_api.get_domain_servers(context, zone_id)
+
+        return self._view.list(context, request, servers, [zone_id])
