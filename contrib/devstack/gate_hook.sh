112d292a27e17115b7a2659c3afdd76adb0682d5
Rich Megginson <rmeggins@redhat.com>
2014-06-03 14:32:26 -0600

    devstack exercise for deletion, dns checking
    This adds record and domain deletion to the designate.sh devstack
    exercise.  This also adds DNS checking for the domain/record operations,
    to verify that the changes were made in DNS.  Some functions in
    designate.sh were moved to lib/designate and refactored slightly.
    This also removes some IPA variables that were refactored into a
    backend specific file, and fixes a bug in the ipa backend file.
    
    Closes-Bug: #1326141
    Change-Id: Ibb86b0b237b3d73a145733bc88dbc18dddbc20f5
    (cherry picked from commit 05291ddbf59f638870a561b1182908b676d92832)

--- contrib/devstack/gate_hook.sh
+++ contrib/devstack/gate_hook.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
-set -e
+set -ex
 
+export KEEP_LOCALRC=1
+
+# Prepare some localrc values
+pushd $BASE/new/devstack
+echo "DESIGNATE_SERVICE_PORT_DNS=5322" >> $BASE/new/devstack/localrc
+popd
+
+# Run DevStack Gate
 $BASE/new/devstack-gate/devstack-vm-gate.sh
--- contrib/devstack/post_test_hook.sh
+++ contrib/devstack/post_test_hook.sh
@@ -2,9 +2,5 @@
 
 set -ex
 
-DESIGNATE_BASE=/opt/stack/new/designate
-DEVSTACK_BASE=/opt/stack/new/devstack
-
 # Run the Designate DevStack exercises
-cd $DEVSTACK_BASE
-./exercises/designate.sh
+$BASE/new/devstack/exercises/designate.sh
