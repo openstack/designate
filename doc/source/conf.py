d44ce8f580916999e1ccc69dde965f45561c6f59
Graham Hayes <graham.hayes@hp.com>
2014-07-07 17:34:17 +0100

    Added oslosphinx theme & enabled the incubating theme
    Added via -f <url> as per https://wiki.openstack.org/wiki/Oslo#Why_aren.27t_alpha_releases_of_oslo.config_published_to_PyPI.3F
    
    Change-Id: I4e35abd6da42a5d9a7bb8c3b56fd4e8610e4e17c

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -25,7 +25,7 @@ import sys, os
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.viewcode', 'sphinxcontrib.httpdomain']
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.viewcode', 'sphinxcontrib.httpdomain', 'oslosphinx']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -97,7 +97,7 @@ html_theme = 'default'
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-#html_theme_options = {}
+html_theme_options = {'incubating': True}
 
 # Add any paths that contain custom themes here, relative to this directory.
 #html_theme_path = []
