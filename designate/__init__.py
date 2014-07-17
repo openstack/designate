0c22ba07994d37a1726303588c4392d193909bdf
Kiall Mac Innes <kiall@hp.com>
2014-07-17 20:20:28 +0100

    Ensure eventlet monkey patching happens as early as possible
    Change-Id: I37822944087385f582181cac3d908287c728d88d
    Closes-Bug: 1343526

--- designate/__init__.py
+++ designate/__init__.py
@@ -13,6 +13,10 @@
 # WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 # License for the specific language governing permissions and limitations
 # under the License.
+import eventlet
+
+eventlet.monkey_patch(os=False)
+
 import os
 import socket
 
