2eabd367173003c66ddefaf7396939d9a8b2d79f
Endre Karlson <endre.karlson@hp.com>
2014-09-09 15:22:15 +0200

    Add index on powerdns.records.designate_record_id
    We need to add the index on this as it avoids some deadlocks.
    
    Change-Id: I82f8bee8f80b1bb410d4e51d8938d0dc92e399c7

---
+++ designate/backend/impl_powerdns/migrate_repo/versions/008_record_add_designate_recordset_id_index.py
@@ -0,0 +1,37 @@
+# Copyright 2013 Hewlett-Packard Development Company, L.P.
+#
+# Author: Endre Karlson <endre.karlson@hp.com>
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+from sqlalchemy import MetaData, Index, Table
+
+meta = MetaData()
+
+
+def upgrade(migrate_engine):
+    meta.bind = migrate_engine
+
+    records_table = Table('records', meta, autoload=True)
+
+    index = Index('designate_recordset_id',
+                  records_table.c.designate_recordset_id)
+    index.create()
+
+
+def downgrade(migrate_engine):
+    meta.bind = migrate_engine
+
+    records_table = Table('records', meta, autoload=True)
+    index = Index('designate_recordset_id',
+                  records_table.c.designate_recordset_id)
+    index.drop()
