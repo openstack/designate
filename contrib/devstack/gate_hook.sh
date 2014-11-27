a91dfb1ffd4a211823cf6ec0b8158855b07888e6
Kiall Mac Innes <kiall@hp.com>
2014-11-27 03:29:52 +0000

    Update gate_hook to support multiple drivers
    The intent here is to begin allowing us to gate on more than
    just PowerDNS.
    
    Change-Id: Ib91a50a18293da672aedd590fc412a97fae3d348

--- contrib/devstack/gate_hook.sh
+++ contrib/devstack/gate_hook.sh
@@ -2,12 +2,24 @@
 
 set -ex
 
+pushd $BASE/new/devstack
+
 export KEEP_LOCALRC=1
 export ENABLED_SERVICES=designate,designate-api,designate-central,designate-sink,designate-mdns
 
-# Prepare some localrc values
-pushd $BASE/new/devstack
 echo "DESIGNATE_SERVICE_PORT_DNS=5322" >> $BASE/new/devstack/localrc
+
+DEVSTACK_GATE_DESIGNATE_DRIVER=${DEVSTACK_GATE_DESIGNATE_DRIVER:-powerdns}
+
+if [ "$DEVSTACK_GATE_DESIGNATE_DRIVER" == "powerdns" ]; then
+    echo "DESIGNATE_BACKEND_DRIVER=powerdns" >> $BASE/new/devstack/localrc
+
+elif [ "$DEVSTACK_GATE_DESIGNATE_DRIVER" == "bind9" ]; then
+    export ENABLED_SERVICES+=,designate-pool-manager
+    echo "DESIGNATE_BACKEND_DRIVER=bind9_pool" >> $BASE/new/devstack/localrc
+
+fi
+
 popd
 
 # Run DevStack Gate
