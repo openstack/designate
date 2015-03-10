72ff534a62329d849b5667c3497c0bfe5f4e60bb
Kiall Mac Innes <kiall@hp.com>
2015-03-10 18:47:08 +0000

    Enable Tempest Functional Tests
    Change-Id: I617c15aa5ba03bec103991f9b65ddc5e3181bd13

--- contrib/devstack/post_test_hook.sh
+++ contrib/devstack/post_test_hook.sh
@@ -4,3 +4,6 @@ set -ex
 
 # Run the Designate DevStack exercises
 $BASE/new/devstack/exercises/designate.sh
+
+# Run the Designate Tempest tests
+sudo ./run_tempest_tests.sh
---
+++ contrib/devstack/run_tempest_tests.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+# How many seconds to wait for the API to be responding before giving up
+API_RESPONDING_TIMEOUT=20
+
+if ! timeout ${API_RESPONDING_TIMEOUT} sh -c "while ! curl -s http://127.0.0.1:9001/ 2>/dev/null | grep -q 'v1' ; do sleep 1; done"; then
+    echo "The Designate API failed to respond within ${API_RESPONDING_TIMEOUT} seconds"
+    exit 1
+fi
+
+echo "Successfully contacted the Designate API"
+
+# Where Designate and Tempest code lives
+DESIGNATE_DIR=${DESIGNATE_DIR:-/opt/stack/new/designate}
+TEMPEST_DIR=${TEMPEST_DIR:-/opt/stack/new/tempest}
+
+# Install tempest
+pip install -e $TEMPEST_DIR
+
+pushd $DESIGNATE_DIR/functionaltests
+
+# run the tests in parallel
+testr init
+testr run --parallel --subunit | subunit-trace --no-failure-debug -f
+retval=$?
+testr slowest
+
+popd
+
+exit $retval
