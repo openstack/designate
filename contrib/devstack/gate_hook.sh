8a3101061e3ee28267e4b2514d85deb45d8a6001
Kiall Mac Innes <kiall@macinnes.ie>
2015-06-30 08:06:21 +0100

    Enable designate-zone-manager in DSVM gates
    Change-Id: I5a95cb0a69f668692da18bfb5d62f2788287f91d

--- contrib/devstack/gate_hook.sh
+++ contrib/devstack/gate_hook.sh
@@ -7,7 +7,9 @@ pushd $BASE/new/devstack
 DEVSTACK_GATE_DESIGNATE_DRIVER=${DEVSTACK_GATE_DESIGNATE_DRIVER:-powerdns}
 
 export KEEP_LOCALRC=1
-export ENABLED_SERVICES=designate,designate-api,designate-central,designate-sink,designate-mdns,designate-pool-manager
+export ENABLED_SERVICES=designate,designate-api,designate-central,designate-sink,designate-mdns,designate-pool-manager,designate-zone-manager
+
+# Remove once we require at least tempest-lib 0.6.0
 export DEVSTACK_PROJECT_FROM_GIT=tempest-lib
 
 echo "DESIGNATE_SERVICE_PORT_DNS=5322" >> $BASE/new/devstack/localrc
