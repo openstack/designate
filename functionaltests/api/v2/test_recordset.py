bc9266a63f57cba0de14fa7a7141265af6adbeab
Endre Karlson <endre.karlson@hp.com>
2015-07-09 10:55:40 +0200

    Increase coverage of RRset tests
    This expand the rrset coverage to include *allmost* all supported types o RTypes.
    
    Change-Id: Idc5363682893804cb7f6330d3a317c46e00d7b0f

--- functionaltests/api/v2/test_recordset.py
+++ functionaltests/api/v2/test_recordset.py
@@ -14,7 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 
-from tempest_lib.exceptions import RestClientException
+from tempest_lib import exceptions
 
 from functionaltests.common import datagen
 from functionaltests.common import utils
@@ -47,9 +47,19 @@ class RecordsetTest(DesignateV2Test):
             make_recordset=lambda z: datagen.random_cname_recordset(z.name)),
         'MX': dict(
             make_recordset=lambda z: datagen.random_mx_recordset(z.name)),
+        'SPF': dict(
+            make_recordset=lambda z: datagen.random_spf_recordset(z.name)),
+        'SRV': dict(
+            make_recordset=lambda z: datagen.random_srv_recordset(z.name)),
+        'SSHFP': dict(
+            make_recordset=lambda z: datagen.random_sshfp_recordset(z.name)),
+        'TXT': dict(
+            make_recordset=lambda z: datagen.random_txt_recordset(z.name)),
+
     })
     def test_crud_recordset(self, make_recordset):
         post_model = make_recordset(self.zone)
+
         resp, post_resp_model = RecordsetClient.as_user('default') \
             .post_recordset(self.zone.id, post_model)
         self.assertEqual(resp.status, 202, "on post response")
@@ -81,6 +91,75 @@ class RecordsetTest(DesignateV2Test):
         RecordsetClient.as_user('default').wait_for_404(
             self.zone.id, recordset_id)
 
+    @utils.parameterized({
+        'A': dict(
+            make_recordset=lambda z: datagen.random_a_recordset(z.name)),
+        'AAAA': dict(
+            make_recordset=lambda z: datagen.random_aaaa_recordset(z.name)),
+        'CNAME': dict(
+            make_recordset=lambda z: datagen.random_cname_recordset(z.name)),
+        'MX': dict(
+            make_recordset=lambda z: datagen.random_mx_recordset(z.name)),
+        'SPF': dict(
+            make_recordset=lambda z: datagen.random_spf_recordset(z.name)),
+        'SRV': dict(
+            make_recordset=lambda z: datagen.random_srv_recordset(z.name)),
+        'SSHFP': dict(
+            make_recordset=lambda z: datagen.random_sshfp_recordset(z.name)),
+        'TXT': dict(
+            make_recordset=lambda z: datagen.random_txt_recordset(z.name)),
+    })
+    def test_create_invalid(self, make_recordset, data=None):
+        data = data or ["b0rk"]
+
+        client = RecordsetClient.as_user('default')
+
+        for i in data:
+            model = make_recordset(self.zone)
+            model.data = i
+            self._assert_exception(
+                exceptions.BadRequest, 'invalid_object', 400,
+                client.post_recordset, self.zone.id, model)
+
+    @utils.parameterized({
+        'A': dict(
+            make_recordset=lambda z: datagen.random_a_recordset(z.name)),
+        'AAAA': dict(
+            make_recordset=lambda z: datagen.random_aaaa_recordset(z.name)),
+        'CNAME': dict(
+            make_recordset=lambda z: datagen.random_cname_recordset(z.name)),
+        'MX': dict(
+            make_recordset=lambda z: datagen.random_mx_recordset(z.name)),
+        'SPF': dict(
+            make_recordset=lambda z: datagen.random_spf_recordset(z.name)),
+        'SRV': dict(
+            make_recordset=lambda z: datagen.random_srv_recordset(z.name)),
+        'SSHFP': dict(
+            make_recordset=lambda z: datagen.random_sshfp_recordset(z.name)),
+        'TXT': dict(
+            make_recordset=lambda z: datagen.random_txt_recordset(z.name)),
+    })
+    def test_update_invalid(self, make_recordset, data=None):
+        data = data or ["b0rk"]
+
+        post_model = make_recordset(self.zone)
+
+        client = RecordsetClient.as_user('default')
+        resp, post_resp_model = client.post_recordset(
+            self.zone.id, post_model)
+
+        recordset_id = post_resp_model.id
+
+        client.wait_for_recordset(
+            self.zone.id, recordset_id)
+
+        for i in data:
+            model = make_recordset(self.zone)
+            model.data = i
+            self._assert_exception(
+                exceptions.BadRequest, 'invalid_object', 400,
+                client.put_recordset, self.zone.id, recordset_id, model)
+
 
 class RecordsetOwnershipTest(DesignateV2Test):
 
@@ -95,13 +174,13 @@ class RecordsetOwnershipTest(DesignateV2Test):
 
         # try with name=A123456.zone.com.
         recordset = datagen.random_a_recordset(zone_name=zone.name)
-        self.assertRaises(RestClientException,
+        self.assertRaises(exceptions.RestClientException,
             lambda: RecordsetClient.as_user('alt')
                     .post_recordset(zone.id, recordset))
 
         # try with name=zone.com.
         recordset.name = zone.name
-        self.assertRaises(RestClientException,
+        self.assertRaises(exceptions.RestClientException,
             lambda: RecordsetClient.as_user('alt')
                     .post_recordset(zone.id, recordset))
 
@@ -114,7 +193,7 @@ class RecordsetOwnershipTest(DesignateV2Test):
         zone_data.name = 'a.b.c.' + zone_data.name
 
         resp, zone = ZoneClient.as_user('default').post_zone(zone_data)
-        self.assertRaises(RestClientException,
+        self.assertRaises(exceptions.RestClientException,
             lambda: RecordsetClient.as_user('alt')
                     .post_recordset(zone.id, recordset))
 
@@ -126,9 +205,9 @@ class RecordsetOwnershipTest(DesignateV2Test):
 
         # alt attempts to create record with name A12345.{zone}
         recordset = datagen.random_a_recordset(zone_name=zone.name)
-        self.assertRaises(RestClientException,
+        self.assertRaises(exceptions.RestClientException,
             lambda: RecordsetClient.as_user('alt')
                     .post_recordset(zone.id, recordset))
-        self.assertRaises(RestClientException,
+        self.assertRaises(exceptions.RestClientException,
             lambda: RecordsetClient.as_user('alt')
                     .post_recordset(alt_zone.id, recordset))
