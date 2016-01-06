8bb77ae26cd3c436ac6fb79aa4d5392f12b7038c
Federico Ceratto <federico.ceratto@hpe.com>
2016-01-06 18:19:15 +0000

    Update periodic-sync-seconds help
    Change-Id: I768c087bdaaa590c41fba7dfc003f767ec6fa143
    Closes-Bug: 1531294

--- designate/pool_manager/__init__.py
+++ designate/pool_manager/__init__.py
@@ -52,8 +52,8 @@ OPTS = [
     cfg.IntOpt('periodic-sync-interval', default=1800,
                help='The time between synchronizing the servers with storage'),
     cfg.IntOpt('periodic-sync-seconds', default=21600,
-               help='Zones Updated within last N seconds will be syncd. Use '
-                    'None to sync all zones.'),
+               help='Zones Updated within last N seconds will be syncd.'
+                    'Use an empty value to sync all zones.'),
     cfg.StrOpt('cache-driver', default='memcache',
                help='The cache driver to use'),
 ]
