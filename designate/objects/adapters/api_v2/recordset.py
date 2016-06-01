129b28a8907c935f5165bdf03f1fb8272c23c9bb
Graham Hayes <graham.hayes@hpe.com>
2016-06-01 16:41:43 +0100

    Add "project-id" to rrset API output
    With the addition of /recordsets we should show the
    project ID for each recordset for admins
    
    APIImpact
    DocImpact
    
    Change-Id: I9a15700fd4fb1fa6f2d14716330501210461a80e

--- designate/objects/adapters/api_v2/recordset.py
+++ designate/objects/adapters/api_v2/recordset.py
@@ -29,6 +29,9 @@ class RecordSetAPIv2Adapter(base.APIv2Adapter):
             "zone_id": {
                 'rename': 'zone_id'
             },
+            "project_id": {
+                'rename': 'tenant_id'
+            },
             "name": {
                 'immutable': True
             },
