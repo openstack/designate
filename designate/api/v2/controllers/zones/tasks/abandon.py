7d6ac0a875ea35227620b8866e2d30ec6e6efa69
Tim Simmons <tim.simmons@rackspace.com>
2016-06-13 21:00:46 +0100

    Ensure HTTP 204 responses have 0 Content-Length
    These calls are misusing Pecan, and result in '' getting
    serialized to JSON, which has a non-empty content-length
    and that's against the rules of HTTP
    
    closes-bug: 1592153
    
    Change-Id: I1407d29c537273b07ab54c4e58c70d9bc4214759
    (cherry picked from commit 3171dd5103aa7295b3947cafc35184ccfb9c9659)

--- designate/api/v2/controllers/zones/tasks/abandon.py
+++ designate/api/v2/controllers/zones/tasks/abandon.py
@@ -20,7 +20,9 @@ from designate.api.v2.controllers import rest
 
 class AbandonController(rest.RestController):
 
-    @pecan.expose(template='json:', content_type='application/json')
+    # NOTE: template=None is important here, template='json:' manifests
+    #       in this bug: https://bugs.launchpad.net/designate/+bug/1592153
+    @pecan.expose(template=None, content_type='application/json')
     @utils.validate_uuid('zone_id')
     def post_all(self, zone_id):
         """Abandon a zone"""
@@ -36,7 +38,6 @@ class AbandonController(rest.RestController):
         else:
             response.status_int = 500
 
-        # NOTE: This is a hack and a half.. But Pecan needs it.
         return ''
 
     @pecan.expose(template='json:', content_type='application/json')
