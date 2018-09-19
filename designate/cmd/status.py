1842802de4a05a057eb7f3c94bd01a4b1f0b9143
Ben Nemec <bnemec@redhat.com>
2018-09-19 20:34:26 +0000

    Add designate-status command for upgrade checks
    This checks for duplicate entries in the service_statuses table
    which will be an error after change
    I307a8f7dd8b8a83effa447a846db3288efa32dba.
    
    Related-Bug: 1768824
    
    Story: 2003657
    Task: 26127
    
    Change-Id: Ie0350b034f0eb03749138aadd0951d30073214c0
    Co-authored-by: Doug Hellmann <doug@doughellmann.com>

---
+++ designate/cmd/status.py
@@ -0,0 +1,57 @@
+# Copyright 2018 Red Hat Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+from oslo_config import cfg
+from oslo_upgradecheck import upgradecheck
+from sqlalchemy import MetaData, Table, select, func
+
+from designate.i18n import _
+from designate.sqlalchemy import session
+# This import is not used, but is needed to register the storage:sqlalchemy
+# group.
+import designate.storage.impl_sqlalchemy  # noqa
+from designate import utils
+
+
+class Checks(upgradecheck.UpgradeCommands):
+    def _duplicate_service_status(self):
+        engine = session.get_engine('storage:sqlalchemy')
+        metadata = MetaData(bind=engine)
+        status = Table('service_statuses', metadata, autoload=True)
+        service_select = (select([func.count()])
+                          .select_from(status)
+                          .group_by('service_name', 'hostname')
+                          )
+        service_counts = engine.execute(service_select).fetchall()
+        duplicated_services = [i for i in service_counts if i[0] > 1]
+        if duplicated_services:
+            return upgradecheck.Result(upgradecheck.Code.FAILURE,
+                                       _('Duplicated services found in '
+                                         'service_statuses table.'))
+        return upgradecheck.Result(upgradecheck.Code.SUCCESS)
+
+    _upgrade_checks = ((_('Duplicate service status'),
+                        _duplicate_service_status),
+                       )
+
+
+def main():
+    config_files = utils.find_config('designate.conf')
+    checker = Checks()
+    return upgradecheck.main(
+        conf=cfg.CONF,
+        project='designate',
+        upgrade_command=checker,
+        default_config_files=config_files,
+    )
---
+++ designate/tests/unit/test_upgrade_checks.py
@@ -0,0 +1,74 @@
+# Copyright 2018 Red Hat Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+from migrate.changeset.constraint import UniqueConstraint
+from oslo_upgradecheck import upgradecheck
+from sqlalchemy.schema import MetaData
+from sqlalchemy.schema import Table
+
+from designate.cmd import status
+from designate import tests
+from designate.sqlalchemy import session
+
+
+class TestDuplicateServiceStatus(tests.TestCase):
+    def setUp(self):
+        super(TestDuplicateServiceStatus, self).setUp()
+        self.engine = session.get_engine('storage:sqlalchemy')
+        self.meta = MetaData()
+        self.meta.bind = self.engine
+        self.service_statuses_table = Table('service_statuses', self.meta,
+                                            autoload=True)
+
+    def test_success(self):
+        fake_record = {'id': '1',
+                       'service_name': 'worker',
+                       'hostname': 'localhost',
+                       'status': 'UP',
+                       'stats': '',
+                       'capabilities': '',
+                       }
+        self.service_statuses_table.insert().execute(fake_record)
+        # Different hostname should be fine
+        fake_record['id'] = '2'
+        fake_record['hostname'] = 'otherhost'
+        self.service_statuses_table.insert().execute(fake_record)
+        # Different service_name should be fine
+        fake_record['id'] = '3'
+        fake_record['service_name'] = 'producer'
+        self.service_statuses_table.insert().execute(fake_record)
+        checks = status.Checks()
+        self.assertEqual(upgradecheck.Code.SUCCESS,
+                         checks._duplicate_service_status().code)
+
+    def test_failure(self):
+        # Drop unique constraint so we can test error cases
+        constraint = UniqueConstraint('service_name', 'hostname',
+                                      table=self.service_statuses_table,
+                                      name="unique_service_status")
+        constraint.drop()
+        fake_record = {'id': '1',
+                       'service_name': 'worker',
+                       'hostname': 'localhost',
+                       'status': 'UP',
+                       'stats': '',
+                       'capabilities': '',
+                       }
+        self.service_statuses_table.insert().execute(fake_record)
+        fake_record['id'] = '2'
+        self.service_statuses_table.insert().execute(fake_record)
+
+        checks = status.Checks()
+        self.assertEqual(upgradecheck.Code.FAILURE,
+                         checks._duplicate_service_status().code)
