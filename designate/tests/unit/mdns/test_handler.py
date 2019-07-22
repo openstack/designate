5e0e4c194bf8ccb17e57c081e061ad580e6d800b
Erik Olof Gunnar Andersson <eandersson@blizzard.com>
2019-07-21 21:10:42 -0700

    Cleaned up mdns handler and added better test coverage
    - Added additional tests.
    - Fixed minor bug with notify log line.
    - Re-factored handle_axfr.
    
    Change-Id: I945c51330dae39f6d7d8384412ed7dc63abd46c1

--- designate/tests/unit/mdns/test_handler.py
+++ designate/tests/unit/mdns/test_handler.py
@@ -15,11 +15,190 @@
 # under the License.
 import dns
 import mock
+from oslo_config import cfg
+from oslo_config import fixture as cfg_fixture
 import oslotest.base
 
 from designate import exceptions
 from designate import objects
 from designate.mdns import handler
+from designate.tests import fixtures
+
+CONF = cfg.CONF
+
+
+class MdnsHandleTest(oslotest.base.BaseTestCase):
+    def setUp(self):
+        super(MdnsHandleTest, self).setUp()
+        self.stdlog = fixtures.StandardLogging()
+        self.useFixture(self.stdlog)
+        self.useFixture(cfg_fixture.Config(CONF))
+        self.context = mock.Mock()
+        self.storage = mock.Mock()
+        self.tg = mock.Mock()
+        self.handler = handler.RequestHandler(self.storage, self.tg)
+
+    @mock.patch.object(dns.resolver.Resolver, 'query')
+    def test_notify(self, mock_query):
+        self.storage.find_zone.return_value = objects.Zone(
+            id='e2bed4dc-9d01-11e4-89d3-123b93f75cba',
+            serial=2,
+            masters=objects.ZoneMasterList.from_list([
+                {'host': '1.0.0.0', 'port': 53},
+            ])
+        )
+        mock_query.return_value = [
+            mock.Mock(serial=1)
+        ]
+
+        request = dns.message.make_query(
+            'www.example.org.', dns.rdatatype.SOA
+        )
+        request.environ = dict(addr=['1.0.0.0'], context=self.context)
+
+        response = self.handler._handle_notify(request)
+
+        self.assertEqual(dns.rcode.NOERROR, tuple(response)[0].rcode())
+
+        self.assertIn(
+            'Scheduling AXFR for e2bed4dc-9d01-11e4-89d3-123b93f75cba '
+            'from 1.0.0.0:53',
+            self.stdlog.logger.output
+        )
+
+    @mock.patch.object(dns.resolver.Resolver, 'query')
+    def test_notify_same_serial(self, mock_query):
+        self.storage.find_zone.return_value = objects.Zone(
+            id='e2bed4dc-9d01-11e4-89d3-123b93f75cba',
+            serial=1,
+            masters=objects.ZoneMasterList.from_list([
+                {'host': '1.0.0.0', 'port': 53},
+            ])
+        )
+        mock_query.return_value = [
+            mock.Mock(serial=1)
+        ]
+
+        request = dns.message.make_query(
+            'www.example.org.', dns.rdatatype.SOA
+        )
+        request.environ = dict(addr=['1.0.0.0'], context=self.context)
+
+        response = self.handler._handle_notify(request)
+
+        self.assertEqual(dns.rcode.NOERROR, tuple(response)[0].rcode())
+
+        self.assertIn(
+            'Serial 1 is the same for master and us for '
+            'e2bed4dc-9d01-11e4-89d3-123b93f75cba',
+            self.stdlog.logger.output
+        )
+
+    def test_notify_no_questions(self):
+        request = dns.message.make_query(
+            'www.example.org.', dns.rdatatype.SOA
+        )
+        request.environ = dict(context=self.context)
+        request.question = []
+
+        response = self.handler._handle_notify(request)
+
+        self.assertEqual(dns.rcode.FORMERR, tuple(response)[0].rcode())
+
+    def test_notify_zone_not_found(self):
+        self.storage.find_zone.side_effect = exceptions.ZoneNotFound
+
+        request = dns.message.make_query(
+            'www.example.org.', dns.rdatatype.SOA
+        )
+        request.environ = dict(context=self.context)
+
+        response = self.handler._handle_notify(request)
+
+        self.assertEqual(dns.rcode.NOTAUTH, tuple(response)[0].rcode())
+
+    def test_notify_no_master_addr(self):
+        self.storage.find_zone.return_value = objects.Zone(
+            masters=objects.ZoneMasterList.from_list([
+                {'host': '1.0.0.0', 'port': 53},
+            ])
+        )
+
+        request = dns.message.make_query(
+            'www.example.org.', dns.rdatatype.SOA
+        )
+        request.environ = dict(addr=['127.0.0.1', 53], context=self.context)
+
+        response = self.handler._handle_notify(request)
+
+        self.assertEqual(dns.rcode.REFUSED, tuple(response)[0].rcode())
+
+        self.assertIn(
+            'NOTIFY for None from non-master server 127.0.0.1, refusing.',
+            self.stdlog.logger.output
+        )
+
+    def test_axfr_zone_not_found(self):
+        self.storage.find_zone.side_effect = exceptions.ZoneNotFound
+
+        request = dns.message.make_query(
+            'www.example.org.', dns.rdatatype.AXFR
+        )
+        request.environ = dict(context=self.context)
+
+        response = tuple(self.handler._handle_axfr(request))
+
+        self.assertEqual(dns.rcode.REFUSED, response[0].rcode())
+
+        self.assertIn(
+            'ZoneNotFound while handling axfr request. '
+            'Question was www.example.org. IN AXFR',
+            self.stdlog.logger.output
+        )
+
+    def test_axfr_forbidden(self):
+        self.storage.find_zone.side_effect = exceptions.Forbidden
+
+        request = dns.message.make_query(
+            'www.example.org.', dns.rdatatype.AXFR
+        )
+        request.environ = dict(context=self.context)
+
+        response = tuple(self.handler._handle_axfr(request))
+
+        self.assertEqual(dns.rcode.REFUSED, response[0].rcode())
+
+        self.assertIn(
+            'Forbidden while handling axfr request. '
+            'Question was www.example.org. IN AXFR',
+            self.stdlog.logger.output
+        )
+
+    def test_get_max_message_size(self):
+        CONF.set_override('max_message_size', 32768, 'service:mdns')
+
+        self.assertEqual(
+            32768, self.handler._get_max_message_size(had_tsig=False)
+        )
+
+    def test_get_max_message_size_larger_than_allowed(self):
+        CONF.set_override('max_message_size', 65535 * 2, 'service:mdns')
+
+        self.assertEqual(
+            65535, self.handler._get_max_message_size(had_tsig=False)
+        )
+
+        self.assertIn(
+            'MDNS max message size must not be greater than 65535',
+            self.stdlog.logger.output
+        )
+
+    def test_get_max_message_with_tsig(self):
+        CONF.set_override('max_message_size', 65535, 'service:mdns')
+
+        self.assertEqual(
+            65300, self.handler._get_max_message_size(had_tsig=True)
+        )
 
 
 class TestRequestHandlerCall(oslotest.base.BaseTestCase):
@@ -138,7 +317,6 @@ class TestRequestHandlerCall(oslotest.base.BaseTestCase):
 
 
 class HandleRecordQueryTest(oslotest.base.BaseTestCase):
-
     def setUp(self):
         super(HandleRecordQueryTest, self).setUp()
         self.context = mock.Mock()
