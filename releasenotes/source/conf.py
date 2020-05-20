663e4206436dc1a9993cf3a43c950a22cf38c436
Andreas Jaeger <aj@suse.com>
2020-05-20 08:10:03 +0200

    Switch to newer openstackdocstheme and reno versions
    Switch to openstackdocstheme 2.2.0 and reno 3.1.0 versions. Using
    these versions will allow especially:
    * Linking from HTML to PDF document
    * Allow parallel building of documents
    * Fix some rendering problems
    
    Update Sphinx version as well.
    
    Set openstackdocs_pdf_link to link to PDF file. Note that
    the link to the published document only works on docs.openstack.org
    where the PDF file is placed in the top-level html directory. The
    site-preview places the PDF in a pdf directory.
    
    Set openstackdocs_auto_name to use 'project' as name.
    
    Change pygments_style to 'native' since old theme version always used
    'native' and the theme now respects the setting and using 'sphinx' can
    lead to some strange rendering.
    
    Remove docs requirements from lower-constraints, they are not needed
    during install or test but only for docs building.
    
    openstackdocstheme renames some variables, so follow the renames
    before the next release removes them. A couple of variables are also
    not needed anymore, remove them.
    
    Add openstackdocstheme options to api-ref.
    
    Change-Id: I0cf71327ca5d9da1d88a826225374c8801849c36

--- releasenotes/source/conf.py
+++ releasenotes/source/conf.py
@@ -42,9 +42,10 @@ extensions = [
 ]
 
 # openstackdocstheme options
-repository_name = 'openstack/designate'
-bug_project = 'designate'
-bug_tag = ''
+openstackdocs_repo_name = 'openstack/designate'
+openstackdocs_bug_project = 'designate'
+openstackdocs_bug_tag = ''
+openstackdocs_auto_name = False
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -100,7 +101,7 @@ exclude_patterns = []
 # show_authors = False
 
 # The name of the Pygments (syntax highlighting) style to use.
-pygments_style = 'sphinx'
+pygments_style = 'native'
 
 # A list of ignored prefixes for module index sorting.
 # modindex_common_prefix = []
