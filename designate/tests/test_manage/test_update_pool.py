a9ab61bc1d98248af108d895ce2cb7696818dcda
Mark Goddard <mark@stackhpc.com>
2020-09-30 17:48:25 +0100

    Fix pool update with duplicate masters
    If any zones exist that belong to a pool that has any duplicate masters
    among all targets, currently updating the pool will fail.
    
    This change fixes the issue by ensuring the pool target master list does
    not contain duplicates.
    
    Change-Id: I72bae79b4f8e0bab739875d6f783abdb4868950b
    Closes-Bug: #1897936
    (cherry picked from commit 4e86bc2a58f00371a2f22217cb8a79e5ac33677c)

--- designate/tests/test_manage/test_update_pool.py
+++ designate/tests/test_manage/test_update_pool.py
@@ -26,11 +26,11 @@ class UpdatePoolTestCase(DesignateManageTestCase):
         self.stdlog = fixtures.StandardLogging()
         self.useFixture(self.stdlog)
 
-    def hydrate_pool_targets(self, targets):
+    def hydrate_pool_targets(self, target_masters):
         pool_targets = objects.PoolTargetList()
         masters = objects.PoolTargetMasterList()
-        for target in targets:
-            masters.append(target)
+        for target_master in target_masters:
+            masters.append(target_master)
         target = objects.PoolTarget(masters=masters)
         target.masters = masters
         pool_targets.append(target)
@@ -63,3 +63,38 @@ class UpdatePoolTestCase(DesignateManageTestCase):
                                "update_zone") as mock_update_zone:
             command._update_zones(pool)
             mock_update_zone.assert_called_once()
+
+    def test_update_pools_zones_multiple_masters(self):
+        values = dict(
+            name='example.com.',
+            email='info@example.com',
+            type='PRIMARY'
+        )
+
+        zone = self.central_service.create_zone(
+            self.admin_context, zone=objects.Zone.from_dict(values))
+
+        # Ensure the correct NS Records are in place
+        pool = self.central_service.get_pool(
+            self.admin_context, zone.pool_id)
+
+        targets1 = self.hydrate_pool_targets([
+            objects.PoolTargetMaster(
+                  pool_target_id=pool.id,
+                  host="127.0.0.1",
+                  port="53")
+        ])
+        targets2 = self.hydrate_pool_targets([
+            objects.PoolTargetMaster(
+                  pool_target_id=pool.id,
+                  host="127.0.0.1",
+                  port="53")
+        ])
+        pool.targets = objects.PoolTargetList()
+        pool.targets.extend(targets1.objects + targets2.objects)
+
+        command = PoolCommands()
+        command.context = self.admin_context
+        command.central_api = self.central_service
+
+        command._update_zones(pool)
