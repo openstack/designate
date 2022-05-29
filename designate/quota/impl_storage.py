291558c380232b726be0253fdee1f88902522beb
Erik Olof Gunnar Andersson <eandersson@blizzard.com>
2022-05-28 20:44:50 -0700

    Fixed incorrect quota exception message
    Change-Id: I38efa22bb8b0de6fd9a70339e8044fea70080004

--- designate/quota/impl_storage.py
+++ designate/quota/impl_storage.py
@@ -61,7 +61,7 @@ class StorageQuota(base.Quota):
 
         if resource not in list(self.get_default_quotas(context).keys()):
             raise exceptions.QuotaResourceUnknown("%s is not a valid quota "
-                                                  "resource", resource)
+                                                  "resource" % resource)
 
         try:
             self._create_quota(context, tenant_id, resource, hard_limit)
