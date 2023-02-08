1f30672bde3148f868fbaece8a58c20413e47b12
Erik Olof Gunnar Andersson <eandersson@blizzard.com>
2023-02-07 19:20:31 -0800

    Fix dns.resolver import for mdns
    We are currently failing with module 'dns' has no
    attribute 'resolver'.
    
    Closes-Bug: #2006537
    Change-Id: I31a1b1892a7112018b1261f956f51ed4eaa7cfb9
    (cherry picked from commit 5cd63481c68ca5d2c8764a93dee5cbb8b45ecf63)

--- designate/mdns/handler.py
+++ designate/mdns/handler.py
@@ -13,13 +13,15 @@
 # WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 # License for the specific language governing permissions and limitations
 # under the License.
-import dns
 import dns.flags
 import dns.message
 import dns.opcode
 import dns.rcode
 import dns.rdataclass
 import dns.rdatatype
+import dns.renderer
+import dns.resolver
+import dns.rrset
 from oslo_config import cfg
 from oslo_log import log as logging
 
