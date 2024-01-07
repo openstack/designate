5c0ef757f595fdca44321f49954b0131965289eb
Erik Olof Gunnar Andersson <eandersson@blizzard.com>
2024-01-07 12:14:05 -0800

    Remove unused validation function
    Change-Id: I30595c472779edccb173b6db0025d7084a192111

--- designate/objects/validation_error.py
+++ designate/objects/validation_error.py
@@ -11,13 +11,14 @@
 #    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 #    License for the specific language governing permissions and limitations
 #    under the License.
+
+
 from designate.objects import base
 from designate.objects import fields
 
 
 @base.DesignateRegistry.register
 class ValidationError(base.DesignateObject):
-
     fields = {
         'path': fields.AnyField(nullable=True),
         'message': fields.AnyField(nullable=True),
@@ -25,22 +26,6 @@ class ValidationError(base.DesignateObject):
         'validator_value': fields.AnyField(nullable=True),
         'raw': fields.AnyField(nullable=True),
     }
-
-    @classmethod
-    def from_js_error(cls, js_error):
-        """Convert a JSON Schema ValidationError instance into a
-        ValidationError instance.
-        """
-
-        e = cls()
-        e.path = list(getattr(js_error, 'releative_path', js_error.path))
-        e.message = js_error.message
-        e.validator = js_error.validator
-        e.validator_value = js_error.validator_value
-
-        e.raw = js_error._contents()
-
-        return e
 
 
 @base.DesignateRegistry.register
