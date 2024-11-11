8a9a79ce7ff7a21f25d7e77034f7959d501955ba
Ghanshyam Mann <gmann@ghanshyammann.com>
2024-11-10 21:38:21 -0800

    Remove default override for config options policy_file
    olso.policy 4.5.0[1] changed the config options policy_file
    default value to 'policy.yaml', which means it is changed
    for all the OpenStack services and they do not need to
    override the default anymore.
    
    NOTE: There is no change in behaviour here, oslo.policy provides
    the same configuration that services have overridden till now.
    
    [1] https://review.opendev.org/c/openstack/releases/+/934012
    [2] https://review.opendev.org/c/openstack/requirements/+/934295
    
    Change-Id: I7228f19f5aa2609a7cc244d3a9a3d850c1d50815

--- designate/common/config.py
+++ designate/common/config.py
@@ -13,9 +13,6 @@
 # under the License.
 
 from oslo_middleware import cors
-from oslo_policy import opts as policy_opts
-
-import designate.conf
 
 
 def set_defaults():
@@ -38,8 +35,3 @@ def set_defaults():
                        'PATCH',
                        'HEAD']
     )
-    # TODO(gmann): Remove setting the default value of config policy_file
-    # once oslo_policy change the default value to 'policy.yaml'.
-    # https://github.com/openstack/oslo.policy/blob/a626ad12fe5a3abd49d70e3e5b95589d279ab578/oslo_policy/opts.py#L49
-    DEFAULT_POLICY_FILE = 'policy.yaml'
-    policy_opts.set_defaults(designate.conf.CONF, DEFAULT_POLICY_FILE)
--- designate/policy.py
+++ designate/policy.py
@@ -15,7 +15,6 @@
 # under the License.
 from oslo_config import cfg
 from oslo_log import log as logging
-from oslo_policy import opts
 from oslo_policy import policy
 
 from designate.common import policies
@@ -24,13 +23,6 @@ from designate import exceptions
 
 
 CONF = designate.conf.CONF
-
-# Add the default policy opts
-# TODO(gmann): Remove setting the default value of config policy_file
-# once oslo_policy change the default value to 'policy.yaml'.
-# https://github.com/openstack/oslo.policy/blob/a626ad12fe5a3abd49d70e3e5b95589d279ab578/oslo_policy/opts.py#L49
-DEFAULT_POLICY_FILE = 'policy.yaml'
-opts.set_defaults(CONF, DEFAULT_POLICY_FILE)
 
 LOG = logging.getLogger(__name__)
 
