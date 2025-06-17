cda679f7ad9082a9a49b0633923183ec2f4fb9a1
Takashi Kajinami <kajinamit@oss.nttdata.com>
2025-06-17 22:32:44 +0900

    Remove workaround for eventlet < 0.27.0
    This code worked around a bug in eventlet[1] that has been fixed in
    6533958f5fcec8b1577f56834ea2dc75a4f211e3. The fix has been available in
    every eventlet release since v0.27.0.
    
    [1] https://github.com/eventlet/eventlet/issues/592
    
    Co-Authored-By: Cyril Roelandt <cyril@redhat.com>
    Change-Id: I9cbf451f32b60e07e83ab248cc729f9cfede37de

--- designate/cmd/manage.py
+++ designate/cmd/manage.py
@@ -31,12 +31,6 @@ from designate import utils
 from designate import version
 
 eventlet.monkey_patch(os=False)
-# Monkey patch the original current_thread to use the up-to-date _active
-# global variable. See https://bugs.launchpad.net/bugs/1863021 and
-# https://github.com/eventlet/eventlet/issues/592
-import __original_module_threading as orig_threading  # noqa
-import threading  # noqa
-orig_threading.current_thread.__globals__['_active'] = threading._active
 
 CONF = designate.conf.CONF
 
